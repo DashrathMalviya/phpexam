48a1189b829de7a0c129fcbb8605a21373aa2147
Dashrath Malviya <dashrathmalviya4@gmail.com>
2020-09-30 12:21:32 +0530

    creating new branc

---
+++ testing/upload.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Examples</title>
+</head>
+<body>
+    <h1>Testing uploadin files</h1>
+    <form action="upload.php" method="POST">
+        <label for="pic">Your pic</label>
+        <input type="file" name="pic" id="pic"><br>
+        <button type="submit">Upload</button>
+    </form>
+</body>
+</html>
